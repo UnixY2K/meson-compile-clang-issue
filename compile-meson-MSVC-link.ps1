95ba35c8c6eaf07f21bb409c552f8ccb35a0e710
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-21 11:54:33 -0600

    use ninja backend

--- compile-meson-MSVC-link.ps1
+++ compile-meson-MSVC-link.ps1
@@ -1,5 +1,7 @@
 # the compiler must be clang++    (default)
 # and the linker must be link.exe (default)
+# to do so install on a clean machine VS(Ex: VS2022) and then llvm
+# if you install another toolchain this might not work
 echo "##########################################"
 echo "configuring directories"
 echo "##########################################"
@@ -14,9 +16,9 @@ mkdir $Folder/libHello > $null
 echo "##########################################"
 echo "Compiling..."
 echo "##########################################"
-# we need to use clang in order to reproduce the issue
-# by default we will use MSVC link, also we cannot specify the linker with MSVC
-meson $Folder
+# by default it will use clang++/link.exe(MSVC)
+meson $Folder --backend ninja
 # this will give LNK1107, as the static library was generated with T(--thin)
 # MSVC link.exe does not support static thin libraries
 meson compile -C $Folder
+## Unset Environment Variables
